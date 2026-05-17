079c7049a8699b7e4746f9d4188e212632b981ab
Jose Maria <josemmoralesleiva@gmail.com>
2026-05-17 13:49:59 +0200

    fix: usar auth.routes.js correcto para registro con telefono y verificacionEstado

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use('/api/auth',     require('./routes/auth'));
+app.use('/api/auth',     require('./routes/auth.routes'));
 app.use('/api/pisos',    require('./routes/pisos'));
 app.use('/api/usuarios', require('./routes/usuarios'));
 
