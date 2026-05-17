3a94c8dc6821d48ff993050785860474ec87b3b8
Jose Maria <josemmoralesleiva@gmail.com>
2026-05-17 13:58:20 +0200

    fix: downgrade bcryptjs a v2.4.3 compatible con CommonJS

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const Usuario = require('../models/Usuario')
 
 const router = express.Router()
